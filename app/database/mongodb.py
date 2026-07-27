3f40ae71e52d289e4e290d82bb103fe32abb5c76
Hrithik <hrithiknonia66@gmail.com>
2026-07-27 17:33:23 +0530

    mongoDB connection complete , lifespan function added for DB connection and close

--- app/database/mongodb.py
+++ app/database/mongodb.py
@@ -1,1 +1,34 @@
+# built in module imports
 from motor.motor_asyncio import AsyncIOMotorClient
+
+
+# custom module imports
+from app.config.setting import setting
+
+
+class MongoDb:
+  client: AsyncIOMotorClient = None
+  database: None
+
+
+mongodb = MongoDb()
+
+
+# data base connection start
+async def connect_to_mongo():
+  mongodb.client = AsyncIOMotorClient(setting.MONGO_URI)
+  mongodb.database = mongodb.client[setting.DATABASE_NAME]
+
+  print(f"✅ Connected to MongoDB: {setting.DATABASE_NAME}")
+
+
+# close data base connection
+async def close_mongodb_connection():
+  if mongodb.client:
+    mongodb.client.close()
+    print("🔌 MongoDB connection closed")
+
+
+# create a function which provide data case connection in entire project
+def get_database():
+  return mongodb.database
--- app/main.py
+++ app/main.py
@@ -1,7 +1,29 @@
+# built in module imports
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 
-app=FastAPI()
+# custom module imports
+from app.database.mongodb import connect_to_mongo, close_mongodb_connection
+from app.config.setting import setting
+
+
+
+# do db connection before starting app and close db connection after app colse 
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+  # connect DB
+  await connect_to_mongo()
+
+  # jab tak app run kar raha hai DB connect rahe ga function pause rahega
+  yield
+
+  # close DB conntction after app close
+  await close_mongodb_connection()
+
+
+
+app=FastAPI(title = setting.PROJECT_NAME, lifespan = app_lifespan)
 
 
 @app.get("/")
